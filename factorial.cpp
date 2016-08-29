87e2d3eb5a7d83e6a29d7e0d5dd893d2071fd010
Nimit <Nimit@nimit.com>
2016-08-29 16:14:05 +0530

    this is first commit of group 10 and nimit

--- factorial.cpp
+++ factorial.cpp
@@ -1,7 +1,11 @@
 #include "functions.h"
 
 int factorial(int n){
-    if(n!=1){
+    if(n<0){
+	std::cout<<"enter number again and carefully\n";
+	return 0;
+    }
+    else if(n!=1){
 	return(n * factorial(n-1));
     }
     else return 1;
