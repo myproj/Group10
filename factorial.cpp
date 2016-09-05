d07c1bcda6042090182f1c6856dc40169617fc70
amanshuraikwar <amanshuraikwar.in@gmail.com>
2016-09-05 16:01:01 +0530

    changed cout to printf according to act-3 step-2

--- factorial.cpp
+++ factorial.cpp
@@ -2,11 +2,11 @@
 
 int factorial(int n){
     if(n<0){
-	std::cout<<"enter number again\n";
-	return 0;
+		printf("enter number again\n");
+		return 0;
     }
     else if(n!=1){
-	return(n * factorial(n-1));
+		return(n * factorial(n-1));
     }
     else return 1;
 }
