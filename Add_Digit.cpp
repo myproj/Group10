177f5dd6d565312fa3f53ad9e46e3200410a0690
varun bansal <varun301296@gmail.com>
2016-09-18 21:09:44 +0530

    with cin cout

--- Add_Digit.cpp
+++ Add_Digit.cpp
@@ -1,23 +1,20 @@
 #include<stdio.h>
- 
+#include<iostream> 
 int main()
 {
    int n, sum = 0, remainder;
- 
-   printf("Enter an integer\n");
-   scanf("%d",&n);
+   cout<<"Enter an integer\n";
+   cin>>n;
     
    while(n != 0)
    {
       remainder = n % 10;
-	  printf("The Remainder is %d\n", remainder);
+	  cout<<"The Remainder is "<<remainder<<"\n";
       sum = sum + remainder;
-	  printf("The Sum is %d\n", sum);
+	  cout<<"The Sum is "<<sum<<"\n";
       n = n / 10;
-	  printf("The n is %d\n", n);
+	  cout<<"The n is "<<n<<"\n";
    }
- 
-   printf("Sum of digits of entered number = %d\n",sum);
- 
+   cout<<"Sum of digits of entered number = "<<sum<<"\n";
    return 0;
 }
